28cdfe86938a26873145f03440b584c839662659
Arash <64580530+4R4S8@users.noreply.github.com>
2021-11-13 15:35:41 +0330

    Update Program.cs

--- long time no see!/Program.cs
+++ long time no see!/Program.cs
@@ -1,53 +1,87 @@
-﻿using System;
+using System;
 
 namespace long_time_no_see_
 {
     class Program
     {
-        static void Main(string[] args)
+        static void The_Thing()
         {
             //درخواست عدد برای محاسبه بخش پذیری
-            Console.WriteLine("Enter The Number :");
+            Console.Write("Enter The Number : ");
             int Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
             //درخواست عدد برای مشخص کردن بازه ی دامنه
-            Console.WriteLine("Enter The Number of Domain :");
-            int Domain = Convert.ToInt32(Console.ReadLine());
-            int s = 0;
-            int j = 0;
-            int kh = 0;
-            for (int i = 0; i < Domain; i++)
+            int First, Last;
+            Console.Write("Enter The Number of Begin Domain : ");
+            First = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("Enter The Number of End Domain : ");
+            Last = Convert.ToInt32(Console.ReadLine());
+            int[] Domain = { First, Last };
+            //مقسوم
+            //int Divisor = 0;
+            //شمارش گر 
+            int Counter = 0;
+            //خارج قسمت
+            int Quotient ;
+            //شمارش اعداد از صفر تا عدد وارد شده در متغیر Domain
+            for (int i=Domain[0]; i <= Domain[1]; i++)
             {
-                s++;
-                if (s%Number==0)
+                //اضافه کردن یک عدد به مقدار قبلیِ مقسوم(متغیر Divisor)
+                Domain[0]++;
+                //اگر باقی مانده صفر بود کارهای زیر را انجام دهد
+                if (Domain[0] % Number == 0)
                 {
-                    kh = s / Number;
-
-                    if (s < 10)
+                    //به دست آوردن خارج قسمت
+                    Quotient = Domain[0] / Number;
+                    //اگر باقی مانده از 10 کوچک تر بود نتیجه زیر را نشان بده
+                    if (Domain[0] < 10)
                     {
-Console.Write(@"
+                        Console.Write(@"
  {0}  |  {1}
     |--------
     |   {2}
     |
     |
-", s, Number,kh);
+", Domain[0], Number, Quotient);
                     }
+                    //در غیر اینصورت نتیجه زیر را نشان بده
                     else
                     {
-                        
-Console.Write(@"
+
+                        Console.Write(@"
  {0} |  {1}
     |--------
     |  {2}
     |
     |
-", s, Number,kh);
+", Domain[0], Number, Quotient);
                     }
-                    j++;
+                    //به ازای چرخش هر حلقه یه مقدار به متغیر شمارش گر اضافه کن
+                    Counter++;
                 }
             }
-            Console.WriteLine("There is {0}",j);
-            Console.ReadKey();
+            //نمایش تعداد اعداد بخش پذیر در بازه مشخص شده
+            Console.WriteLine("There is {0} Divisible between {1} and {2}", Counter,Domain[0] ,Domain[1]);
+            Console.WriteLine();
+            Console.Write("Do ypu want to continue[y,n]? ");
+            char choose = Convert.ToChar(Console.ReadLine());
+            if (choose == 'y' || choose == 'Y')
+            {
+                 The_Thing();
+            }
+            else if (choose == 'n' || choose == 'N')
+            {
+                Console.WriteLine("See ya later!");
+            }
+            else
+            {
+                Console.ReadKey();
+            }
+        }
+        static void Main(string[] args)
+        {
+            The_Thing();       
         }
     }
 }
